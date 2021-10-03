d7690a98192c78e8329a72998669b97252b8bd2a
Patrick K <patrick.kitchens1@gmail.com>
2021-10-02 22:52:21 -0400

    changed auto open to firefox

--- config/webpack.develop.js
+++ config/webpack.develop.js
@@ -14,7 +14,7 @@ module.exports = merge(common, {
   // Spin up a server for quick development
   devServer: {
     historyApiFallback: true,
-    open: true,
+    open: 'firefox',
     compress: true,
     hot: true,
     port: 8080,
